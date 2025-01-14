2e9e20a00be51b21e090bfee98de42df7182229c
Gcettbdeveloper <associationmembers6699@gmail.com>
2025-01-14 18:32:13 +0530

    group model updated

--- models/groups.model.js
+++ models/groups.model.js
@@ -8,6 +8,21 @@ const groupSchema = new mongoose.Schema(
             required: true,
         },
 
+        // reference to the user model
+        users: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User",
+            },
+        ],
+
+        expenses: [
+            {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Expense' // Reference to the Expense schema
+            }
+        ]
+
     },
     {
         timestamps: true
