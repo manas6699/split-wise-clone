6aadee885bdaf04eb3113df4e92b6bf3361d0e0b
Gcettbdeveloper <associationmembers6699@gmail.com>
2025-01-14 18:32:57 +0530

    extra auth middleware added

--- routes/expenses.routes.js
+++ routes/expenses.routes.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const authMiddleware = require('../middlewares/auth.middleware');
 
 const addExpenseHandler = require('../controllers/addExpense.controller')
 
 const router = express.Router();
 
 
-router.route("/expenses").post(authMiddleware , addExpenseHandler);
+router.route("/expenses").post(addExpenseHandler);
 
 
 module.exports = router;
