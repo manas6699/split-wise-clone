c4bfb8ff6b254fdf412daacf33b8da0af227f61d
manas6699 <manasbaroi6699@gmail.com>
2025-06-02 16:17:28 +0530

    deleting cookie in logout handler logic added

--- controllers/logout.controller.js
+++ controllers/logout.controller.js
@@ -1,10 +1,18 @@
-const logoutHandler = async(req , res) => {
-     const token = req.headers.authorization?.split(' ')[1];
+const logoutHandler = async (req, res) => {
+  const token = req.headers.authorization?.split(' ')[1];
   if (!token) {
     return res.status(400).json({ message: 'Token is required for logout.' });
   }
 
-  res.status(200).json({ message: 'Logged out successfully.' });
-}
+  // Clear the token cookie
+  res.clearCookie('token', {
+    httpOnly: true,       // if it was set as httpOnly
+    secure: true,         // if using HTTPS
+    sameSite: 'Lax',      // match how it was set
+    path: '/',            // match the cookie's path
+  });
 
-module.exports = logoutHandler
+  return res.status(200).json({ message: 'Logged out successfully.' });
+};
+
+module.exports = logoutHandler;
