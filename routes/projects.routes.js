c7018a7fdb4c5213eee5fc59ea7e01a764bbb4d4
manas6699 <manasbaroi6699@gmail.com>
2025-07-02 17:46:06 +0530

    testing purpose

--- routes/projects.routes.js
+++ routes/projects.routes.js
@@ -7,10 +7,13 @@ const {
   updateProject
 } = require('../controllers/addProject.controller');
 
-const { uploadProjectAssets } = require('../middlewares/upload.middleware');
+const { uploadProjectAssets, uploadMixed } = require('../middlewares/upload.middleware');
 
 // ✅ Route to create a new project with file uploads
-router.post('/create', uploadProjectAssets, createProject);
+router.post('/create', uploadMixed.any(), (req, res) => {
+  console.log('Received file fieldnames:', req.files.map(f => f.fieldname));
+  return res.send('Check console');
+});
 
 // ✅ Get project by ID
 router.get('/:id', getProjectById);
