b5304404e5175d934ac4fe891ea7386d332a4f58
manas6699 <manasbaroi6699@gmail.com>
2025-07-16 12:39:26 +0530

    register controller unique name logic removed

--- controllers/register.controller.js
+++ controllers/register.controller.js
@@ -1,17 +1,17 @@
 const asyncHandler = require("../utils/asynchandler");
-const bcrypt = require('bcrypt');
+
 const User = require('../models/user.model');
 
 const registerUser = asyncHandler(async(req , res) => {
-     const { name, password, phone } = req.body;
+     const { name, password, phone , role } = req.body;
   console.log(req.body)
   try {
     console.log("entered try-catch block");
     // Check if user exists
-    const existingUser = await User.findOne({ $or: [{ name }, { phone }] });
+    const existingUser = await User.findOne({ phone });
     if (existingUser) return res.status(400).json({ message: 'User already exists' });
     console.log("user is not existing")
-    const user = new User({ name,  password, phone });
+    const user = new User({ name,  password, phone , role });
     await user.save();
 
     res.status(201).json({ message: 'User registered.' });
