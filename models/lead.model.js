2cd7503faf2795661ff9b69ce1891762a747f165
manas6699 <manasbaroi6699@gmail.com>
2025-07-24 13:33:49 +0530

    Update lead schema to include additional status options and restructure fields for clarity

--- models/lead.model.js
+++ models/lead.model.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+
 const leadSchema = new mongoose.Schema({
 
 
@@ -16,15 +17,22 @@ phone: {
     unique: true,
     required: [true, 'Phone number is required'],
 },
+
 source:{
     type: String,
     required: true,
 },
 status: {
     type: String,
-    enum: ['assigned', 'not-assigned'],
+    enum: ['assigned', 'not-assigned' , 'reassigned' , 'auto-assigned'],
     default: 'not-assigned',
 },
+assigned_to: {
+    type: mongoose.Schema.Types.ObjectId , ref: 'User'
+},
+assignee_name:{
+    type:mongoose.Schema.Types.String, ref: 'User'
+}
 },
 {
     timestamps:true
