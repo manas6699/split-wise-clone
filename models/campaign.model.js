b614dddc5f90580569798581ab61e9947d7bbf8e
manas6699 <manasbaroi6699@gmail.com>
2025-07-24 13:33:07 +0530

    Format campaign schema for improved readability

--- models/campaign.model.js
+++ models/campaign.model.js
@@ -1,10 +1,22 @@
 const mongoose = require('mongoose');
 
 const campaignSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  source: { type: String, required: true },
-  telecallers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  auto_assign: { type: Boolean, default: false }
+  name: { 
+    type: String, 
+    required: true 
+  },
+  source: { 
+    type: String, 
+    required: true 
+  },
+  telecallers: [{ 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User' 
+  }],
+  auto_assign: { 
+    type: Boolean, 
+    default: false 
+  }
 });
 
 module.exports = mongoose.model('Campaign', campaignSchema);
