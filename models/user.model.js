210725c943cfdf7ca0a09887d0c49ce89ab5db9b
manas6699 <manasbaroi6699@gmail.com>
2025-08-05 12:16:25 +0530

    fcm token added in user model

--- models/user.model.js
+++ models/user.model.js
@@ -17,6 +17,9 @@ const userSchema = new mongoose.Schema(
             type: String,
             required: [true, 'Password is required']
         },
+        fcmToken: {
+            type: String,
+        },
         role: {
             type: String,
             enum: ['admin', 'telecaller', 'salesperson', 'supervisor'],
