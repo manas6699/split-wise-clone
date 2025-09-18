7b8780ada4b6b18f205b24a9b79844fd41b31f19
manas6699 <manasbaroi6699@gmail.com>
2025-09-18 19:17:56 +0530

    feat: add bulk upload and assign leads route

--- routes/bulkLeadUpload.routes.js
+++ routes/bulkLeadUpload.routes.js
@@ -1,7 +1,7 @@
 // routes/leadRoutes.js
 const express = require("express");
 const multer = require("multer");
-const { bulkUploadLeads } = require("../controllers/bulkLeadUpload.controller");
+const { bulkUploadLeads , bulkUploadAndAssignLeads } = require("../controllers/bulkLeadUpload.controller");
 
 const router = express.Router();
 
@@ -10,5 +10,12 @@ const upload = multer({ dest: "tmp/csv/" });
 
 // Bulk upload route
 router.post("/bulk", upload.single("file"), bulkUploadLeads);
+
+// bulk upload + assign leads
+router.post(
+  "/bulk-upload-assign",
+  upload.single("file"),
+  bulkUploadAndAssignLeads
+);
 
 module.exports = router;
